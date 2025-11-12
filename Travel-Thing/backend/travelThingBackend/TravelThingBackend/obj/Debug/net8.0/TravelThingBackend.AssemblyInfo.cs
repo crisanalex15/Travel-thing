5b08f2a2d587ab31a2e2c8ce24602e9030e8d5fb
crisanalex15 <alex.crisan@student.upt.ro>
2025-11-12 16:10:30 +0200

    Incepem treaba

--- Travel-Thing/backend/travelThingBackend/TravelThingBackend/obj/Debug/net8.0/TravelThingBackend.AssemblyInfo.cs
+++ Travel-Thing/backend/travelThingBackend/TravelThingBackend/obj/Debug/net8.0/TravelThingBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelThingBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c56c04430b02eeca3fc2e505e992c202c1ae33fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+069779913ae0552fee211406de6e8b6215ca702e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelThingBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelThingBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
